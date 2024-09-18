3bb8eb5b8eebceaa256b2dcb339ecf8eb14a51db
Maxim Pachuzheuski <makspohp123@gmail.com>
2024-09-18 19:57:12 +0300

    feat:finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let openBrackets = {};
+  let closeBrackets = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let open = bracketsConfig[i][0];
+    let close = bracketsConfig[i][1];
+    openBrackets[open] = close;
+    closeBrackets[close] = open;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (openBrackets[char]) {
+      if (openBrackets[char] === char && stack[stack.length - 1] === char) {
+        stack.pop(); 
+      } else {
+        stack.push(char);
+      }
+    } 
+    else if (closeBrackets[char]) {
+      let lastOpen = stack.pop(); 
+      if (lastOpen !== closeBrackets[char]) {
+        return false; 
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
